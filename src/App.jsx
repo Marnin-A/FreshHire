4c151de51b62d54cc7149e8a3286093c3ed011e8
Marnin Audu <msmaudu@gmail.com>
2023-03-19 20:25:07 +0100

    Add pages folder, and home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import { Home } from "./pages/home/home";
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to Fresh Hire👋</h1>
+      <Home />
     </div>
   );
 }
