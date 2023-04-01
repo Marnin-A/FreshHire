8433440ae9277b81f659ab89b43809cdb9598438
Marnin Audu <msmaudu@gmail.com>
2023-04-01 16:12:13 +0100

    Increase navbar vertical margins, and create footer file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Home } from "./pages/home";
 import NavBar from "./components/navBar/navBar";
+import Footer from "./components/footer/footer";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Home />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "../footer/footer.module.css";
+
+export default function Footer() {
+  return <div className={styles.footer}>Footer</div>;
+}
