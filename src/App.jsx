82d03e81a26b0ba4a5525c1fcf20151efc94071a
Marnin Audu <msmaudu@gmail.com>
2023-04-10 16:12:43 +0100

    Change Router implementation due to routing

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,20 @@ import About from "./pages/about/about";
 import ContactUs from "./pages/contactUs/ContactUs";
 import NavBar from "./components/navBar/navBar";
 import Footer from "./components/footer/footer";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  const router = createBrowserRouter([
-    { path: "FreshHire/", element: <Home /> },
-    { path: "FreshHire/about", element: <About /> },
-    { path: "FreshHire/contact_us", element: <ContactUs /> },
-  ]);
   return (
     <div className="App">
-      <NavBar />
-      <RouterProvider router={router} />
-      <Footer />
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="FreshHire/" element={<Home />} />
+          <Route path="FreshHire/about" element={<About />} />
+          <Route path="FreshHire/contact_us" element={<ContactUs />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
