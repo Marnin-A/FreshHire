2972ba5f5f38aea9adcd70c8379530c2b40dd0bb
zinoJ <joshuaubiri@gmail.com>
2023-05-13 11:03:37 +0100

    navbar responsiveness

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -1,26 +1,70 @@
-import React from "react";
-import styles from "./navBar.module.css";
+import React, { useState } from "react";
+// import styles from "./navBar.module.css";
+import "./navBar.css";
 import Logo from "../../assets/Fresh_hire.png";
+import Hamburger from "../../assets/hamburger_menu.png";
 import LogoText from "../../assets/Fresh_hire_light.png";
 
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function NavBar() {
+  const navigate = useNavigate();
+  const [click, setClick] = useState(false);
+  const [scroll, setScroll] = useState(false)
+  const handleClick = () => {
+    setClick(!click)
+  };
+
+  const addBoxShadow = () => {
+    if (window.scrollY >= 100) {
+      setScroll(true)
+    } else {
+      setScroll(false)
+    }
+  }
+
+  window.addEventListener('scroll', addBoxShadow)
+
   return (
-    <div className={styles.nav_Bar}>
-      <div className={styles.logo}>
-        <img src={Logo} alt="Site Logo" />
-        Freshire
+    <div className={scroll ? "header onScroll" : "header"}>
+      <div className="logo">
+        <img src={Logo} alt="FreshHire Logo" onClick={() => navigate("/")} style={{ cursor: "pointer" }} /> Freshire
       </div>
-      <div className={styles.nav_Links}>
-        <Link to="FreshHire/">Home</Link>
-        <Link to="FreshHire/about">About us</Link>
-        <Link to="FreshHire/contact_us">Contact us</Link>
-        <Link to="FreshHire/post_job">Post a job</Link>
-        <Link to="FreshHire/job_listing">Job listings</Link>
-      </div>
-      <div className={styles.signup_area}>
+      <ul
+        className={click ? "nav-menu active" : "nav-menu"}
+        onClick={handleClick}
+      >
+        <li>
+          <Link to="FreshHire/">Home</Link>
+        </li>
+        <li>
+          <Link to="FreshHire/about">About Us</Link>
+        </li>
+        <li>
+          <Link to="FreshHire/contact_us">Contact Us</Link>
+        </li>
+        <li>
+          <Link to="FreshHire/post_job">Post a job</Link>
+        </li>
+        <li>
+          <Link to="FreshHire/job_listings">
+            Job Listings
+          </Link>
+        </li>
+        <li className="signup_area_resp">
+          <div className="signup_area">
+            <button>Sign up</button>
+          </div>
+        </li>
+      </ul>
+      <div className="signup_area hide">
         <button>Sign up</button>
+      </div>
+      <div
+        className="hamburger"
+        onClick={handleClick}
+      >
+        {click ? <p>X</p> : <img src={Hamburger} alt="" />}
       </div>
     </div>
   );
