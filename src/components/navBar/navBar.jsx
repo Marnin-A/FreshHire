b536f0cefc9aebb3fe485d08722ef309fc82c6d7
zinoJ <joshuaubiri@gmail.com>
2023-05-13 14:10:19 +0100

    cherry-pick responsiveness commit from main to production branches

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import styles from "./navBar.module.css";
 import Logo from "../../assets/Fresh_hire.png";
-import LogoText from "../../assets/Fresh_hire_light.png";
 
 import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <div className={styles.nav_Bar}>
-      <div className={styles.logo}>
-        <img src={Logo} alt="Site Logo" />
-        Freshire
+    <div className={scroll ? "header onScroll" : "header"}>
+      <div
+        className="logo"
+        onClick={() => navigate("FreshHire/")}
+        style={{ cursor: "pointer" }}
+      >
+        <img src={Logo} alt="FreshHire Logo" /> Freshire
       </div>
       <div className={styles.nav_Links}>
         <Link to="/">Home</Link>
