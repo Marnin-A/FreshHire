7ea2d86e29deaae54a69375f3f78c45158d95303
Marnin-A <msmaudu@gmail.com>
2023-05-21 22:06:02 +0100

    Add coming soon popup

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -1,5 +1,8 @@
-import React, { useState } from "react";
-// import styles from "./navBar.module.css";
+import * as React from "react";
+import { useState } from "react";
+import Popover from "@mui/material/Popover";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 import "./navBar.css";
 import Logo from "../../assets/Fresh_hire.png";
 import Hamburger from "../../assets/hamburger_menu.png";
@@ -11,8 +14,16 @@ export default function NavBar() {
   const navigate = useNavigate();
   const [click, setClick] = useState(false);
   const [scroll, setScroll] = useState(false);
+  const [anchorEl, setAnchorEl] = React.useState(null);
   const handleClick = () => {
     setClick(!click);
+  };
+  const handleOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
   };
 
   const addBoxShadow = () => {
@@ -25,6 +36,8 @@ export default function NavBar() {
 
   window.addEventListener("scroll", addBoxShadow);
 
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
   return (
     <div className={scroll ? "header onScroll" : "header"}>
       <div
@@ -60,7 +73,25 @@ export default function NavBar() {
         </li>
       </ul>
       <div className="signup_area hide">
-        <button>Sign up</button>
+        <Button aria-describedby={id} variant="contained" onClick={handleOpen}>
+          Sign up
+        </Button>
+        <Popover
+          id={id}
+          open={open}
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "center",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "center",
+          }}
+        >
+          <Typography sx={{ p: 2 }}>Coming soon...</Typography>
+        </Popover>
       </div>
       <div className="hamburger" onClick={handleClick}>
         {click ? <p>X</p> : <img src={Hamburger} alt="" />}
