0a3412d1c4e04973b599f79631b82cba4dadbabd
Marnin-A <msmaudu@gmail.com>
2023-05-26 20:11:52 +0100

    Add coming soon to hero

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,10 +1,41 @@
 import { useNavigate } from "react-router-dom";
+import * as React from "react";
+import { useState } from "react";
 import heroImage from "../../assets/hero__image.webp";
 import Wavy_Check from "../../assets/Wavy_Check.png";
+import Popover from "@mui/material/Popover";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 import "./Hero.css";
 
 const Hero = () => {
   const navigate = useNavigate();
+  const [click, setClick] = useState(false);
+  const [scroll, setScroll] = useState(false);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const handleClick = () => {
+    setClick(!click);
+  };
+  const handleOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const addBoxShadow = () => {
+    if (window.scrollY >= 100) {
+      setScroll(true);
+    } else {
+      setScroll(false);
+    }
+  };
+
+  window.addEventListener("scroll", addBoxShadow);
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
   return (
     <div className="hero">
       <div className="hero__container">
@@ -15,7 +46,29 @@ const Hero = () => {
             talents.
           </p>
           <div className="hero__left__postjob">
-            <button>Post Job</button>
+            <Button
+              aria-describedby={id}
+              variant="contained"
+              onClick={handleOpen}
+            >
+              Post Job
+            </Button>
+            <Popover
+              id={id}
+              open={open}
+              anchorEl={anchorEl}
+              onClose={handleClose}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "center",
+              }}
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "center",
+              }}
+            >
+              <Typography sx={{ p: 2 }}>Coming soon...</Typography>
+            </Popover>
             <p>Apply for jobs</p>
           </div>
           <div className="hero__left__bottom">
